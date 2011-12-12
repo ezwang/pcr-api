7cff52a579693fabc7f1820645900c420030dfe9
Alexey Komissarouk <alexeymk@gmail.com>
2011-12-12 18:57:45 -0500

    added (inactive for now) buttons in admin for cache generation

--- api/courses/admin.py
+++ api/courses/admin.py
@@ -1,12 +1,23 @@
 from courses.models import *
 from django.contrib import admin
+from buttonable_model_admin import *
+
+
+class CourseAdmin(ButtonableModelAdmin):
+  def generate_cache(self, what_is_this_other_arg):
+    pass
+
+  generate_cache.short_description = 'Generate Cache'
+
+  buttons = [generate_cache]
 
 
 class InstructorAdmin(admin.ModelAdmin):
   search_fields = ['first_name', 'last_name']
 
+
 admin.site.register(Instructor, InstructorAdmin)
+admin.site.register(Course, CourseAdmin)
 
-for model in [Course, Section]:
+for model in [Section]:
   admin.site.register(model)
-
