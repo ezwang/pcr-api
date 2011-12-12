7f467423189b4edc0d442cc58f7f4c782a2c47ee
Ceasar Bautista <ceasarb@seas.upenn.edu>
2011-12-12 15:38:11 -0500

    Wrote skelton for courses/tests

--- api/courses/tests.py
+++ api/courses/tests.py
@@ -1,23 +1,170 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
+Test for the API views.
 """
+import json
 
 from django.test import TestCase
+from django.test.client import Client
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from api.apiconsumer.models import APIConsumer
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+import models
 
->>> 1 + 1 == 2
-True
-"""}
 
+class DataTest(TestCase):
+  def setUp(self):
+    self.consumer = APIConsumer.objects.create(
+        name="test",
+        email="test@test.com",
+        description="",
+        token="root",
+        permission_level=9001
+        )
+    self.client = Client()
+
+    cis = models.Department.objects.create(
+        code='CIS', name='Computer Science')
+    ese = models.Department.objects.create(
+        code='ESE', name='Electrical Engineering')
+    cis110 = models.CourseHistory.objects.create(notes="None.")
+    cis110_1 = models.Course.objects.create(
+        semester = 810,
+        name = "CIS 110",
+        credits = 1.0,
+        description = "intro to CS.",
+        history = cis110
+        )
+    instructor1 = models.Instructor.objects.create(
+        first_name="John", last_name="Doe")
+    instructor2 = models.Instructor.objects.create(
+        first_name="Jane", last_name="Doe")
+    section1 = models.Section.objects.create(
+        course=cis110_1, name="Intro to CS", 
+        sectionnum='001', sectiontype='LEC')
+    alias1 = models.Alias.objects.create(
+        course=cis110_1, department=cis, coursenum=1, semester='810')
+    alias2 = models.Alias.objects.create(
+        course=cis110_1, department=ese, coursenum=1, semester='810')
+    review1 = models.Review.objects.create(
+        section=section1, instructor=instructor1,
+        forms_returned=10, forms_produced=20, form_type=1,
+        comments="Students enjoyed the course.")
+    reviewbit1 = models.ReviewBit.objects.create(
+        review=review1, field="Instructor Quality", score=3.0)
+    reviewbit2 = models.ReviewBit.objects.create(
+        review=review1, field="Difficulty", score=1.0)
+    reviewbit3 = models.ReviewBit.objects.create(
+        review=review1, field="Course Quality", score=2.0)
+
+  def get_result(self, path):
+    """Load a page on the API and return the result.
+    
+    Fails on inability to parse json or invalid content.
+    """
+    response = self.client.get(path, {'token': self.consumer.token})
+    try:
+      content = json.loads(response.content)
+    except ValueError as e:
+      self.fail("%s (s=\"%s\")" % (e, response.content))
+    self.assertTrue(content['valid'], content)
+    return content['result']
+
+  def test_presence_of_depts(self):
+    result = self.get_result('/depts/')
+    values = result['values']
+    self.assertTrue(len(values) > 0, msg=result)
+
+  def test_dept_should_have(self):
+    result = self.get_result('/depts/CIS')
+    self.assertTrue("coursehistories" in result)
+    self.assertTrue("id" in result)
+    self.assertTrue("name" in result)
+    self.assertTrue("path" in result)
+    self.assertTrue("reviews" in result)
+  
+  def test_coursehistory_should_have(self):
+    result = self.get_result('/coursehistories/1')
+    self.assertTrue("aliases" in result)
+    self.assertTrue("courses" in result)
+    self.assertTrue("id" in result)
+    self.assertTrue("name" in result)
+    self.assertTrue("path" in result)
+    self.assertTrue("reviews" in result)
+
+  def test_presence_of_instructors(self):
+    result = self.get_result('/instructors/')
+    values = result['values']
+    self.assertTrue(len(values) > 0, msg=result)
+
+  def test_course_should_have(self):
+    result = self.get_result('/courses/1')
+    self.assertTrue("aliases" in result)
+    self.assertTrue("coursehistories" in result)
+    self.assertTrue("credits" in result)
+    self.assertTrue("description" in result)
+    self.assertTrue("id" in result)
+    self.assertTrue("name" in result)
+    self.assertTrue("path" in result)
+    self.assertTrue("reviews" in result)
+    self.assertTrue("sections" in result)
+    self.assertTrue("semester" in result)
+
+  def test_presence_of_course_reviews(self):
+    result = self.get_result('/courses/1/reviews')
+    values = result['values']
+    self.assertTrue(len(values) > 0, msg=result)
+
+  def test_presence_of_sections(self):
+    result = self.get_result('/courses/1/sections/')
+    values = result['values']
+    self.assertTrue(len(values) > 0, msg=result)
+
+  def test_section_should_have(self):
+    result = self.get_result('/courses/1/sections/001/')
+    self.assertTrue("aliases" in result)
+    self.assertTrue("courses" in result)
+    self.assertTrue("group" in result)
+    self.assertTrue("id" in result)
+    self.assertTrue("instructors" in result)
+    self.assertTrue("meetingtimes" in result)
+    self.assertTrue("name" in result)
+    self.assertTrue("path" in result)
+    self.assertTrue("reviews" in result)
+    self.assertTrue("sectionnum" in result)
+
+  def test_presence_of_section_reviews(self):
+    result = self.get_result('/courses/1/sections/001/reviews')
+    values = result['values']
+    self.assertTrue(len(values) > 0, msg=result)
+
+  def test_review_should_have(self):
+    result = self.get_result('/courses/1/sections/001/reviews')
+    review = result['values'][0]
+
+    self.assertTrue("comments" in review)
+    self.assertTrue("id" in review)
+    self.assertTrue("instructor" in review)
+    self.assertTrue("num_reviewers" in review)
+    self.assertTrue("num_students" in review)
+    self.assertTrue("path" in review)
+    self.assertTrue("ratings" in review)
+    self.assertTrue("section" in review)
+
+    comments = review['comments']
+    self.assertTrue(comments != "null", msg=comments)
+    self.assertTrue(len(comments) > 0, msg=comments)
+
+    instructor = review['instructor']
+    self.assertTrue(instructor['name'] == "John Doe", msg=instructor)
+
+    ratings = review['ratings']
+    self.assertTrue(len(ratings) > 0, msg=result)
+
+    num_reviewers = review['num_reviewers']
+    self.assertTrue(int(num_reviewers) > 0)
+
+    num_students = review['num_students']
+    self.assertTrue(int(num_students) > 0)
+
+    section = review['section']
+    self.assertTrue(section['sectionnum'] == '001')
