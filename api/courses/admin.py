9ffe8e1fc79cb82d2da384a7c6c504214855e092
Alexey Komissarouk <alexeymk@gmail.com>
2011-12-12 19:01:05 -0500

    removed buttonable model admin (it never worked)

--- api/courses/admin.py
+++ api/courses/admin.py
@@ -1,23 +1,10 @@
 from courses.models import *
 from django.contrib import admin
-from buttonable_model_admin import *
-
-
-class CourseAdmin(ButtonableModelAdmin):
-  def generate_cache(self, what_is_this_other_arg):
-    pass
-
-  generate_cache.short_description = 'Generate Cache'
-
-  buttons = [generate_cache]
-
 
 class InstructorAdmin(admin.ModelAdmin):
   search_fields = ['first_name', 'last_name']
 
-
 admin.site.register(Instructor, InstructorAdmin)
-admin.site.register(Course, CourseAdmin)
 
-for model in [Section]:
+for model in [Course, Section]:
   admin.site.register(model)
