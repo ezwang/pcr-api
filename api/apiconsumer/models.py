9e32265d9f8c8a8e148255b3fb486737c0eb0766
Alexey Komissarouk <alexeymk@gmail.com>
2011-12-16 19:03:24 -0500

    more readable apiconsumer

--- api/apiconsumer/models.py
+++ api/apiconsumer/models.py
@@ -5,8 +5,8 @@ class APIConsumer(models.Model):
   email = models.EmailField(max_length=75, unique=True)
   description = models.TextField()
   token = models.CharField(max_length=200, unique=True)
-
   permission_level = models.IntegerField()
+
   # 0 - no access, equivalent to no key
   # 1 - access to public data only
   # 2 - access to PCR data
@@ -24,3 +24,5 @@ class APIConsumer(models.Model):
   def access_secret(self):
     return self.permission_level > 9000
   
+  def __unicode__(self):
+    return "%s (level %d)" % (self.name, self.permission_level)
