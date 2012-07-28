2d40671e2213b5ee0b6ec143349927fcd3f2d1c0
Kyle Hardgrave <kyle@kylehardgrave.com>
2012-07-28 18:07:27 -0400

    Change depcreated ADMINT_MEDIA_PREFIX to STATIC_URL.

--- api/settings.py
+++ api/settings.py
@@ -63,7 +63,7 @@ MEDIA_URL = ''
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/pcradmin/media/'
+STATIC_URL = '/pcradmin/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'F2g3XtfUb76T5g-PWNdIKYeD4ajuvz6tVLrDQLVddw5hVr7bnVGygYNUrYWGCNYs1'
