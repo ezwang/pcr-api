43b1246458a1b0d8b614c76d59efe2d8e2bdecd2
Kyle Hardgrave <kyle@kylehardgrave.com>
2012-07-29 20:42:13 -0400

    Move resusable functions into generic Test class for API views

--- api/courses/tests.py
+++ api/courses/tests.py
@@ -1,5 +1,7 @@
 """
-Test for the API views.
+Tests for the API views.
+
+These are run by `./manage.py test`.
 """
 import json
 
@@ -11,8 +13,20 @@ from models import (Alias, Course, CourseHistory, Department, Instructor,
                     Section, Semester, Review, ReviewBit)
 
 
-class DataTest(TestCase):
+class ViewTest(TestCase):
+  """A test of the API views (i.e., the JSON served by the API).
+
+  Attributes:
+      consumer: An APIConsumer object, for querying the API.
+      client: A Django test client, for making basic HTTP calls.
+  """
+
   def setUp(self):
+    """Set up the necessary objects for testing.
+
+    If overrided in a subclass, be sure to call
+    `super(ChildTest, self).setUp()` to create the consumer and client.
+    """
     self.consumer = APIConsumer.objects.create(
         name="test",
         email="test@test.com",
@@ -21,6 +35,28 @@ class DataTest(TestCase):
         permission_level=9001
         )
     self.client = Client()
+
+
+  def get_result(self, path):
+    """Load a page on the API and return the result.
+
+    Fails on inability to parse json or invalid content.
+    """
+    response = self.client.get(path, {'token': self.consumer.token})
+    try:
+      content = json.loads(response.content)
+    except ValueError as e:
+      self.fail("%s (s=\"%s\")" % (e, response.content))
+    self.assertTrue(content['valid'], content)
+    return content['result']
+
+
+
+class DataTest(ViewTest):
+  """Test the basic integrety of the API structure."""
+
+  def setUp(self):
+    super(DataTest, self).setUp()
 
     cis = Department.objects.create(
         code='CIS', name='Computer Science')
@@ -54,20 +90,8 @@ class DataTest(TestCase):
     reviewbit3 = ReviewBit.objects.create(
         review=review1, field="Course Quality", score=2.0)
 
-  def get_result(self, path):
-    """Load a page on the API and return the result.
-    
-    Fails on inability to parse json or invalid content.
-    """
-    response = self.client.get(path, {'token': self.consumer.token})
-    try:
-      content = json.loads(response.content)
-    except ValueError as e:
-      self.fail("%s (s=\"%s\")" % (e, response.content))
-    self.assertTrue(content['valid'], content)
-    return content['result']
-
   def validate_results(self, path):
+    """Check that there are, in fact, results."""
     result = self.get_result(path)
     self.assertTrue(len(result['values']) > 0, result)
 
