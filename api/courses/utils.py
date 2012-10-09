03a9ee4cc1a02efcb7af45ccd254a2f957a1a9a3
Ceasar Bautista <cbautista2010@gmail.com>
2012-10-08 21:31:03 -0400

    Make utils file

---
+++ api/courses/utils.py
@@ -0,0 +1,9 @@
+import datetime
+
+from models import Semester
+
+
+def current_semester():
+  now = datetime.datetime.now()
+  semester = 'A' if now.month < 5 else ('B' if now.month < 9 else 'C')
+  return Semester(now.year, semester)
