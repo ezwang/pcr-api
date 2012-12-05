df7612ee8382dbed6406210bf9be729d1f8bb589
Kyle Hardgrave <kyle@kylehardgrave.com>
2012-12-05 17:59:38 -0500

    Removed nyancats. You're welcome, @Ceasar

--- api/json_helpers.py
+++ api/json_helpers.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 def JSON(x, valid=True, httpstatus=200):
   jsonstr = json.dumps(
-    {"result": x, "valid": valid, "version": "0.3", "nyan": "cats",
+    {"result": x, "valid": valid, "version": "0.3",
      "retrieved": str(datetime.datetime.now())},
     sort_keys=True,
     indent=3)
