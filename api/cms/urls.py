de3366ffd55242cad4e4d7e15fccd0e92bd52ddf
Clara Wu <s.clara.wu@gmail.com>
2013-04-22 19:49:48 -0400

    user api working, url takes in parameter

--- api/cms/urls.py
+++ api/cms/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
-    (r'^users', users),
+    (r'^users/(?P<userid>\d+)/?$', users),
     (r'^index.html', home),
     (r'^course.html', course),
     (r'^edit.html', edit),
