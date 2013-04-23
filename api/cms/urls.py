0a9ca797dc69789402226a8ca1efbbdba466517c
Clara Wu <s.clara.wu@gmail.com>
2013-04-22 21:54:49 -0400

    ajax calls available, initial loads, updating courses, updating assignments, updating tags

--- api/cms/urls.py
+++ api/cms/urls.py
@@ -4,6 +4,7 @@ from views import *
 
 urlpatterns = patterns('',
     (r'^users/(?P<userid>\d+)/?$', users),
+    (r'^initial/?$', initial),
     (r'^index.html', home),
     (r'^course.html', course),
     (r'^edit.html', edit),
--- api/cms/views.py
+++ api/cms/views.py
@@ -26,24 +26,68 @@ def users(request, userid):
             #create a summary dictionary that will be an attribute of the course dict
             s = {'status': summary.status, 'text' : summary.text}
             i = {'summary': s, 'name' : co.name, 'department' : co.department,
-                 'professor' : co.professor, 'section' : co.section}
+                 'professor' : co.professor, 'section' : co.section, 'courseid' : co.id}
             c.append(i)
         u['courses'] = c
         data = json.dumps(u)
         return HttpResponse(data)
-    # writers = []
-    # full_users = UserProfile.objects.filter(user_type='WR')
-    # for u in full_users:
-    #     i = {'email' : u.user.email, 'userid' : u.id, 'name': u.user.first_name + u.user.last_name}
-    #     tags = u.tags.all()
-    #     tag_list= [t.category for t in tags]
-    #     i['tags'] = tag_list
-    #     writers.append(i)
-    # if request.method == 'GET':
-    #     data  = json.dumps(writers)
-    #     return HttpResponse(data)
-    # else:
-    #     return render_to_response('cms/users.html', {writers : writers}, context_instance = RequestContext(request))
+
+
+def initial(request):
+    all_info = {}
+    writers = []
+    #gets all writers of type "writer"
+    full_users = UserProfile.objects.filter(user_type='WR')
+    for u in full_users:
+        i = {'email' : u.user.email, 'userid' : u.id, 'name': u.user.first_name + " " + u.user.last_name}
+        tags = u.tags.all()
+        #get all tags of user
+        tag_list= [t.category for t in tags]
+        i['tags'] = tag_list
+        #add writer information to list of writers
+        writers.append(i)
+    all_info['users'] = writers
+    courses = []
+    #get all courses
+    all_courses = Course.objects.all()
+    for co in all_courses:
+        i = {'name' :  co.name, 'department' : co.department,
+             'professor' : co.professor, 'section' : co.section, 'courseid' : co.id}
+        courses.append(i)
+    all_info['courses'] = courses
+    #put into json dump and return
+    data = json.dumps(all_info)
+    return HttpResponse(data)
+
+def update_assignments(request):
+    if request.is_ajax():
+        course_id = request.POST['courseid']
+        user_id = request.POST['userid']
+        user = UserProfile.objects.get(id=user_id)
+        if user:
+            course = Course.objects.get(id=course_id)
+            if course:
+                course.user = user
+                course.save()
+                return HttpResponse('{"Response":"Success", "Results": "True"}')
+            else:
+                return HttpResponse('{"Response":"Failure", "Results": "No course found"}')
+        else:
+            return HttpResponse('{"Response":"Failure", "Results": "No user found"}')
+    else:
+        return HttpResponse('{"Response":"Failure", "Results": "Not an AJAX request"}')
+
+def add_tags(request):
+    if request.is_ajax():
+        tag = request.POST['tag']
+        user_id = request.POST['userid']
+        user = UserProfile.objects.get(id=user_id)
+        if user:
+            if not user.tags.objects.filter(category = tag).exists():
+                tag = Tag.objects.get(category = tag)
+                user.tags.add(tag)
+                user.save()
+                return HttpResponse('{"Response":"Success", "Results": "True"}')
 
 
 def course(request):
