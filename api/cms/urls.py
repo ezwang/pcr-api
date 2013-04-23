aabe9235836df29db82f29078136bb7b1294e6a1
Clara Wu <s.clara.wu@gmail.com>
2013-04-22 21:56:35 -0400

    updated urls

--- api/cms/urls.py
+++ api/cms/urls.py
@@ -5,6 +5,8 @@ from views import *
 urlpatterns = patterns('',
     (r'^users/(?P<userid>\d+)/?$', users),
     (r'^initial/?$', initial),
+    (r'^_add_tag/?$', add_tags),
+    (r'^_update_assigments/?$', update_assignments),
     (r'^index.html', home),
     (r'^course.html', course),
     (r'^edit.html', edit),
