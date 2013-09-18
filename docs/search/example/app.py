2a64ee51b236511905588f078022fc2088f8a08a
Ceasar Bautista <cbautista2010@gmail.com>
2013-09-17 22:08:13 -0400

    Add search spec

---
+++ docs/search/example/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    with open("index.html") as f:
+        return f.read()
+
+
+@app.route('/countries.json')
+def countries():
+    with open("countries.json") as f:
+        return f.read()
+
+
+@app.route('/courses.json')
+def countries():
+    with open("courses.json") as f:
+        return f.read()
+
+if __name__ == '__main__':
+    app.run(debug=True)
