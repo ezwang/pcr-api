72109a9b0ffeadedc626a5df1fbe01fb5af1b6c5
Eric Wang <ezwang1@gmail.com>
2017-10-31 21:12:01 -0400

    better logging

--- scripts/download.py
+++ scripts/download.py
@@ -5,6 +5,7 @@ them into /registrar.'''
 
 import re
 import time
+import logging
 import urllib2, re, codecs, os, shutil
 
 from bs4 import BeautifulSoup
@@ -48,18 +49,20 @@ os.chdir('./registrardata')
 for urlpart, subject in subjects:
     url = rosterUrl + urlpart
     file_name = subject + ".txt"
-    print file_name, url
-    #if file already exists, just skip this one for now
+    # if file already exists, just skip this one for now
     if not os.path.isfile(file_name):
         try:
             soup = BeautifulSoup(urllib2.urlopen(url).read(), "html.parser")
         except urllib2.HTTPError:
-            print "Failed!"
+            logging.warning("Failed to parse %s", url)
             continue
         outfile = open(file_name, 'w')
         outfile.write(subject.encode('utf-8') + '\n')
         outfile.write(soup.pre.text)
         outfile.close()
+        logging.info("Added %s from %s", file_name, url)
         # should not be necessary anymore, the whole operation
         # is finished in less than a minute without any problems
         # time.sleep(3)  # don't get all angry at me for scraping yo
+    else:
+        logging.warning("%s already exists, skipping...", file_name)
